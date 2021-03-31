510c4d006d986c33dfb1d665c9bb8d4ce7968eec
rajendra99999 <81578333+rajendra99999@users.noreply.github.com>
2021-03-31 17:30:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -41,29 +41,10 @@ def predict():
     prediction = model.predict(final_input)
     output=round(prediction[0],2)
     
-    if output>30:
-        pred='''Air Quality index in Your area is {} : SATISFACTORY level of air quality - 
-        May cause minor breathing discomfort to sensitive people.'''.format(output)
-    elif output>60:
-        pred='''Air Quality index in Your area is {} : MODERATELY POLLUTED level of air quality - 
-        May cause breathing discomfort to people with lung disease such as asthma, 
-        and discomfort to people with heart disease, children and older adults.'''.format(output)
-    elif output>90:
-        pred='''Air Quality index in Your area is {} : POOR level of air quality - 
-        May cause breathing discomfort to people on prolonged exposure, 
-        and discomfort to people with heart disease.'''.format(output)
-    elif output>120:
-        pred='''Air Quality index in Your area is {} : VERY POOR level of air quality - 
-        May cause respiratory illness to the people on prolonged exposure. 
-        Effect may be more pronounced in people with lung and heart diseases.'''.format(output)
-    elif output>250:
-        pred='''Air Quality index in Your area is {} : SEVERE poluted level of air quality - 
-        May cause respiratory impact even on healthy people, and serious health impacts on people with lung/heart disease. 
-        The health impacts may be experienced even during light physical activity.'''.format(output)
-    else:
-        pred='''Air Quality index in Your area is {} : Congrats!! GOOD quality Air - Minimal health impact.'''.format(output)
     
-    return render_template('index.html', prediction_text=pred)
+       
+    
+    return render_template('index.html', 'Air Quality index in Your area is {}'.format(output))
 
 if __name__=='__main__':
     app.run(debug=True)
